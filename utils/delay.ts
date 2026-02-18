0e806ac9b12aa849df0b20c8f394242ec99e4d55
Jonathan McLemore <58408019+Jmx097@users.noreply.github.com>
2026-02-18 15:22:29 -0500

    Delete utils directory

--- utils/delay.ts
+++
@@ -1,4 +0,0 @@
-export function humanDelay(min = 800, max = 1800) {
-  const delay = Math.floor(Math.random() * (max - min + 1)) + min;
-  return new Promise(resolve => setTimeout(resolve, delay));
-}
